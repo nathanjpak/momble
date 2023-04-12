8399bfb7fcb47367eb8db628e24216d735de61b7
nathanjpak <nathan.j.pak@gmail.com>
2023-04-12 13:40:59 -0700

    color code notifications

--- client/src/components/Multiplayer/PlayerCards/Notification.tsx
+++ client/src/components/Multiplayer/PlayerCards/Notification.tsx
@@ -1,11 +1,17 @@
 import { TNotification } from ".";
 
 export default function Notification({ data }: { data: TNotification }) {
+  const bgClass =
+    "relative text-white text-center py-2 px-6 w-5/6 max-w-[10rem] min-w-[min-content] left-1/2 -translate-x-1/2 -translate-y-12";
+  const bgPointerClass =
+    "before:absolute before:w-0 before:h-0 before:top-full before:left-4 before:border-8 before:border-transparent";
+  const bgColorClass =
+    data.correct || data.points
+      ? "bg-emerald-300 before:border-t-emerald-300"
+      : "bg-red-300 before:border-t-red-300";
+
   return (
-    <p
-      className="relative bg-emerald-300 text-white text-center py-2 px-6 w-5/6 max-w-[10rem] min-w-[min-content] left-1/2 -translate-x-1/2 -translate-y-12 before:absolute before:w-0 before:h-0 before:top-full before:left-4 before:border-8 before:border-transparent before:border-t-emerald-300"
-      key={data.id}
-    >
+    <p className={`${bgClass} ${bgPointerClass} ${bgColorClass}`} key={data.id}>
       {data.points !== undefined ? `${data.msg} +${data.points}pts` : data.msg}
     </p>
   );
