df32de039d7ab3805e1d8576f56aeff00190fb54
nathanjpak <nathan.j.pak@gmail.com>
2023-04-12 18:00:34 -0700

    link to go back

--- client/src/components/Multiplayer/Header.tsx
+++ client/src/components/Multiplayer/Header.tsx
@@ -1,12 +1,20 @@
+import { Link, useNavigate } from "react-router-dom";
+
 export default function MultiplayerHeader({
   game,
 }: {
   game: string | undefined;
 }) {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <div className="w-screen flex justify-center">
-        <p>{game?.toUpperCase()}</p>
+      <div className="w-screen flex justify-between">
+        <text className="cursor-pointer" onClick={() => navigate(-1)}>
+          Go back
+        </text>
+        <Link to="../..">Momble</Link>
+        <p className="self-center">{game?.toUpperCase()}</p>
       </div>
     </div>
   );
